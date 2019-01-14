6c439e8c91582907cdf41c2d40b3eb1f7b45e15d
alex <you@example.comyou@example.com>
2019-01-14 23:07:41 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { AbouteComponent } from './component/aboute/aboute.component';
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'about', component:AbouteComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AbouteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
